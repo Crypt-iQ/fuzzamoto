0ce3268f85ed313ee3a3b428d6296aa1bdace19a
Eugene Siegel <elzeigel@gmail.com>
2025-11-06 10:29:01 -0500

    targets: add -sharetemplatecount argument

--- fuzzamoto/src/targets/bitcoin_core.rs
+++ fuzzamoto/src/targets/bitcoin_core.rs
@@ -72,6 +72,7 @@ impl Target<V1Transport> for BitcoinCoreTarget {
             "-dbcache=4",    // 4MiB
             "-datacarriersize=1000000",
             "-peertimeout=8223372036854775807",
+            "-sharetemplatecount=10",
         ]);
 
         let node = Node::with_conf(exe_path, &config)
