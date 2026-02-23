1d327923e3784460ca8ef6d2c797c287486441b9
Eugene Siegel <elzeigel@gmail.com>
2026-02-23 16:17:29 -0300

    perf18

--- fuzzamoto-scenarios/bin/ir.rs
+++ fuzzamoto-scenarios/bin/ir.rs
@@ -571,7 +571,7 @@ where
         let mut program = testcase.program;
         let mut start_index = 0;
 
-        let mut program_len = program.actions.len();
+        //let mut program_len = program.actions.len();
 
         while let Some((new_payload, action_pos)) =
             self.process_actions(program, start_index, runner)
@@ -634,7 +634,7 @@ where
             program = new_testcase.program;
             start_index = action_pos;
 
-            program_len = program.actions.len();
+            //program_len = program.actions.len();
         }
 
         self.ping_connections();
